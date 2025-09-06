0e268a6fcc2b0ead06a307193c64e91a64577340
Luke Ponga <developmentdesignsltd@gmail.com>
2025-09-06 02:46:57 +0000

    <!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/theme-provider';
 import { AppLayout } from '@/components/app-layout';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'JobTrack Pro',
@@ -20,8 +21,23 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PMJPBTJ7');`}
+        </Script>
       </head>
       <body className="font-sans antialiased h-full bg-background">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PMJPBTJ7"
+            height="0" 
+            width="0" 
+            style={{display:'none', visibility:'hidden'}}
+          ></iframe>
+        </noscript>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
