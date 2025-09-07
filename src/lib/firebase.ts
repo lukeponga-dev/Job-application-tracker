757ab00fd6654cdeb69a8a3df3a5d11fd7bd6d21
Luke Ponga <developmentdesignsltd@gmail.com>
2025-09-07 03:09:04 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // You should populate this with your own Firebase project configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_STACK_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_STACK_PROJECT_ID}.appspot.com`,
+  messagingSenderId: "", // Optional, can be left empty
+  appId: "", // Optional, can be left empty
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
